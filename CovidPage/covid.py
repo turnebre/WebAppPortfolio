3a04922fc5a64c6009fa70daaee2d1ee59da5bd2
Brendan <brendanhturner@gmail.com>
2022-01-23 23:40:31 -0500

    removing data ingestion for dashboard

--- CovidPage/covid.py
+++ CovidPage/covid.py
@@ -1,78 +1,8 @@
-from cProfile import run
-from email import message
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
-from datetime import datetime, date, timedelta, time as dtime
-import time
-from gspread_pandas import Spread
 
 
 def make_covid_dashboard():
-    def load_data():
-        loading = st.info("Loading data from source")
-        covid_data_raw = pd.read_csv(
-            "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv",
-            parse_dates=["date"],
-        )
-
-        loading.empty()
-
-        return covid_data_raw
-
-    def clean_data(raw_data: pd.DataFrame()):
-        """
-        Cleans data by removing uneeded columns and dropping continents.
-        Also interpolates vaccination data due to inconsitencies each day.
-        """
-        cleaning = st.info("Cleaning data")
-        covid_data_raw = raw_data.loc[~raw_data["continent"].isna(), :]
-
-        regex = """total|excess|aged|extreme|capita|cardiovasc|\
-            |smokers|tests|index|expectancy|beds|density|\
-                |handwashing|diabetes|median|positive|reproduction|\
-                    |iso_code|continent|weekly|per_million|per_hundred|smoothed"""
-
-        dropped_columns = list(covid_data_raw.filter(regex=regex))
-
-        booster_column = ["total_boosters"]
-
-        covid_data = covid_data_raw.copy()[covid_data_raw.columns.drop(dropped_columns)]
-
-        covid_data[booster_column] = covid_data_raw[booster_column]
-
-        interp_columns = [
-            "people_fully_vaccinated",
-            "people_vaccinated",
-            "total_boosters",
-        ]
-
-        covid_data_interp = pd.DataFrame()
-
-        locations = covid_data["location"].unique()
-
-        for location in locations:
-            location_set = covid_data.copy().loc[covid_data["location"] == location, :]
-            location_set = location_set.sort_values(["location", "date"])
-            location_set[interp_columns] = location_set[interp_columns].interpolate()
-            location_set.iloc[:, 2:] = location_set.iloc[:, 2:].fillna(0)
-            covid_data_interp = covid_data_interp.copy().append(location_set)
-
-        cleaning.empty()
-
-        return covid_data_interp
-
-    def export_data(cleaned_data: pd.DataFrame()):
-        """
-        Uploads data to Google Sheets and schedules the next run for 4pm the following day.
-        """
-        exporting = st.info("Exporting to Google Sheets")
-
-        spread = Spread("COVID Dashboard")
-        spread.df_to_sheet(
-            cleaned_data, index=False, sheet="Dashboard Data", start="A1", replace=True,
-        )
-        exporting.empty()
 
     dashboard = components.html(
         """
@@ -104,35 +34,3 @@ def make_covid_dashboard():
     """,
         height=800,
     )
-
-    if datetime.now().time() >= dtime(9, 30):
-        run_date = datetime.now().date() + timedelta(days=1)
-        runtime = datetime.combine(run_date, dtime(9, 30))
-        print("Next Runtime: " + str(runtime))
-    else:
-        runtime = datetime.combine(datetime.now().date(), dtime(9, 30))
-        print("Next Runtime: " + str(runtime))
-
-    now = datetime.now()
-    if now >= runtime:
-        dashboard.empty()
-        progress = 0
-        my_bar = st.progress(progress)
-
-        raw_data = load_data()
-        progress += 1 / 3
-        my_bar.progress(progress)
-
-        cleaned_data = clean_data(raw_data)
-        progress += 1 / 3
-        my_bar.progress(progress)
-
-        export_data(cleaned_data)
-        progress += 1 / 3
-        my_bar.progress(progress)
-
-        success = st.success("Successfully exported data")
-        time.sleep(3)
-        success.empty()
-        my_bar.empty()
-        dashboard
